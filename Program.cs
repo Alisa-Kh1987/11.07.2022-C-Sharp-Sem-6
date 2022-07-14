7b04eaf7f00b00c4af1740e71cae492dc05e416e
Алиса <petrunina.alisa@gmail.com>
2022-07-14 20:19:22 +0300

    add comment

--- Program.cs
+++ Program.cs
@@ -67,3 +67,63 @@ Console.WriteLine(String.Join(" ", array));
 
 
 //Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+//Практическое задание к семинару № 6 (11.07.2022)
+
+//
+/*
+int[] CreateRandomArray(int N, int start, int end)
+{
+int[] RandomArray = new int[N];
+for (int i = 0; i < N; i++)
+{
+RandomArray[i] = new Random().Next(start, end + 1);
+}
+return RandomArray;
+}
+*/
+int[] CreateArray()
+{
+Console.WriteLine("Введите количество элементов массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] RandomArray = new int[size];
+for (int i = 0; i < size; i++)
+{
+Console.WriteLine($"Введите {i+1} элемент массива");
+RandomArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
+
+Console.Clear();
+/*Console.WriteLine("Введите количество элементов в массиве");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число случайно генерируемого диапазона");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
+int max = Convert.ToInt32(Console.ReadLine());
+*/
+//int[] myRandomArray = CreateRandomArray(num, min, max);
+//ShowArray(myRandomArray);
+Console.WriteLine("-------");
+int[] myArray = CreateArray();
+ShowArray(myArray);
+
+int positiv_nums = 0;
+for (int i = 0; i<myArray.Length; i++)
+{
+if (myArray[i] > 0){
+    positiv_nums += 1;
+    }    
+}
+
+Console.WriteLine(positiv_nums);
